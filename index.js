603c457d8711f04d5ce30defbf6e367169a08013
Lucas Gancia <94696289+gancialucas@users.noreply.github.com>
2024-07-16 14:21:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// Dicee 1
+let randomNumber1 = Math.floor(Math.random() * 6) + 1
+document.querySelector(".img1").setAttribute("src", "./images/dice"+ randomNumber1 +".png")
+
+// Dicee 2
+let randomNumber2 = Math.round(Math.random() * 5) + 1
+document.querySelector(".img2").setAttribute("src", "./images/dice"+ randomNumber2 +".png")
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Winer ¡Player 1! 🎉"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Winer ¡Player 2! 🎉"
+} else {
+    document.querySelector("h1").textContent = "¡Draw! Try again"
+}

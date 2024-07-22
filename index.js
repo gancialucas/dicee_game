97955294da7690ae55f6ffa362854626375be7e0
Lucas Gancia <94696289+gancialucas@users.noreply.github.com>
2024-07-22 19:15:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ let randomNumber2 = Math.round(Math.random() * 5) + 1
 document.querySelector(".img2").setAttribute("src", "./images/dice"+ randomNumber2 +".png")
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Winer ¡Player 1! 🎉"
+    document.querySelector("h1").textContent = "Winner ¡Player 1! 🎉"
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "Winer ¡Player 2! 🎉"
+    document.querySelector("h1").textContent = "Winner ¡Player 2! 🎉"
 } else {
     document.querySelector("h1").textContent = "¡Draw! Try again"
 }
